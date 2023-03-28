dda9c9003aaa5a1cef5e4864ebeeb4dccd2c37ef
Gregory Omelchuk <gregoryomelchuk@vk.com>
2023-03-28 18:01:27 +0300

    a few more cosmetic changes

--- app/src/main/java/com/example/nofoodsharingproject/fragments/getter/GetterAdvrsF.java
+++ app/src/main/java/com/example/nofoodsharingproject/fragments/getter/GetterAdvrsF.java
@@ -8,7 +8,9 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.example.nofoodsharingproject.R;
@@ -24,6 +26,15 @@ public class GetterAdvrsF extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState){
+        ImageButton goToMap = getActivity().findViewById(R.id.open_map);
+        TextView adresShop = getActivity().findViewById(R.id.adress_shop);
+        TextView activeAdvertisement = getActivity().findViewById(R.id.on_advertisement);
+        String mewmessage = "Это объявление сейчас не активно";
+        Button b = getActivity().findViewById(R.id.create_new_request);
+        b.setText(mewmessage);
+
+
+
         return inflater.inflate(R.layout.fragment_getter_advrs, container, false);
     }
 }
