a0f37dd694888b74cffeb3a197b32b31df631050
Gregory Omelchuk <gregoryomelchuk@vk.com>
2023-03-28 18:33:30 +0300

    all bad - anything not work. Poshel varit pelmeni

--- app/src/main/java/com/example/nofoodsharingproject/fragments/getter/GetterAdvrsF.java
+++ app/src/main/java/com/example/nofoodsharingproject/fragments/getter/GetterAdvrsF.java
@@ -1,5 +1,6 @@
 package com.example.nofoodsharingproject.fragments.getter;
 
+import android.content.Intent;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 
@@ -13,6 +14,7 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+import com.example.nofoodsharingproject.MainActivity;
 import com.example.nofoodsharingproject.R;
 
 public class GetterAdvrsF extends Fragment {
@@ -26,15 +28,13 @@ public class GetterAdvrsF extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState){
+        View view = inflater.inflate(R.layout.fragment_getter_advrs, container, false);
         ImageButton goToMap = getActivity().findViewById(R.id.open_map);
         TextView adresShop = getActivity().findViewById(R.id.adress_shop);
         TextView activeAdvertisement = getActivity().findViewById(R.id.on_advertisement);
-        String mewmessage = "Это объявление сейчас не активно";
-        Button b = getActivity().findViewById(R.id.create_new_request);
-        b.setText(mewmessage);
 
 
 
-        return inflater.inflate(R.layout.fragment_getter_advrs, container, false);
+        return view;
     }
 }
