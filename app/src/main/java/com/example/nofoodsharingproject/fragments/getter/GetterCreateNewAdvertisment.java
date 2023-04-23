092582cbaa1ad213a2d43590806c54f85da3f148
glortest <gregoryomelchuk@vk.com>
2023-04-23 21:58:24 +0300

    koroche ocherednoy komit s kakoy-to figney

--- app/src/main/java/com/example/nofoodsharingproject/fragments/getter/GetterCreateNewAdvertisment.java
+++ app/src/main/java/com/example/nofoodsharingproject/fragments/getter/GetterCreateNewAdvertisment.java
@@ -8,8 +8,10 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.nofoodsharingproject.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class GetterCreateNewAdvertisment extends Fragment {
     @Override
@@ -21,10 +23,9 @@ public class GetterCreateNewAdvertisment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState){
-        View view = inflater.inflate(R.layout.fragment_getter_new_advertisment, container, false);
-
-
-
+        View view = inflater.inflate(R.layout.fragment_getter_create_new_advertisment, container, false);
+        Button buttonReadyCreate = view.findViewById(R.id.ready_to_create);
+        RecyclerView recyclerView =view.findViewById(R.id.recycler);
 
         return view;
     }
