a57b0d6ddeab839f3ef0856c7d28ee325b69f292
IFramex <kulsasha55@gmail.com>
2023-05-01 22:45:09 +0300

    подготовка к разработке системы уведомлений. Изменение gradle настроек и обновление всех пакетов

---
+++ app/src/main/java/com/example/nofoodsharingproject/utils/CustomMessagingService.java
@@ -0,0 +1,5 @@
+package com.example.nofoodsharingproject.utils;
+
+//import com.google.firebase.messaging.FirebaseMessagingService;
+//public class CustomMessagingService extends FirebaseMessagingService {
+//}
